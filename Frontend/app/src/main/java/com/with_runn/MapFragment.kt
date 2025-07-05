d1b7cc20b01e7f7916b584e72d02ea0d105bbf85
Medorpond <134402610+Medorpond@users.noreply.github.com>
2025-07-05 20:18:47 +0900

    [Feat]: Chip 생성 코드 적용
    - MapFragment에 더미 키워드 추가
    - MapFragment에 더미 콜백 함수 추가
    - MapFragment에서 Chip버튼 생성 함수 호출

--- Frontend/app/src/main/java/com/with_runn/MapFragment.kt
+++ Frontend/app/src/main/java/com/with_runn/MapFragment.kt
@@ -1,16 +1,20 @@
 package com.with_runn
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.chip.Chip
 import com.with_runn.databinding.FragmentMapBinding
 
 class MapFragment : Fragment() {
 
     private var _binding : FragmentMapBinding? = null;
     private val binding get() = _binding!!
+
+    private val keywords = listOf("병원", "약국", "반려동물용품", "미용", "식당", "당식", "용미", "품용물동려반", "국약", "원병")
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,7 +27,13 @@ class MapFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        populateChips(
+            chipGroup = binding.chipGroup,
+            inflater = layoutInflater,
+            keywords = keywords
+        ){
+            chip -> onChipClick(chip)
+        }
     }
 
     override fun onDestroyView() {
@@ -31,4 +41,8 @@ class MapFragment : Fragment() {
         _binding = null;
     }
 
+    fun onChipClick(chip: Chip){
+        Log.d("ChipClick", "${chip.text} Clicked")
+        // TODO: Naver API에 키워드로 검색 요청해서 결과 표시하기
+    }
 }
