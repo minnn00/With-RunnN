e085680d2ffaa38d279406de682e42511b6f40a7
Medorpond <134402610+Medorpond@users.noreply.github.com>
2025-07-19 17:02:55 +0900

    [ADD] Retrofit, okhttp 및 GSON 의존성 추가
    - retrofit
    - retrofit gson
    - okhttp
    - okhttp-logging
    - gson
    추가

--- Frontend/app/build.gradle.kts
+++ Frontend/app/build.gradle.kts
@@ -61,6 +61,11 @@ dependencies {
     implementation(libs.navigation.ui.ktx)
     implementation(libs.speed.dial)
     implementation(libs.naver.maps)
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.gson)
+    implementation(libs.okhttp)
+    implementation(libs.okhttp.logging)
+    implementation(libs.gson)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
