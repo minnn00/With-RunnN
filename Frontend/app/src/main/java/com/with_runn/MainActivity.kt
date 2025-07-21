ce5c36388f8cb0915680d27682c7291c2525c917
Medorpond <134402610+Medorpond@users.noreply.github.com>
2025-07-21 13:28:38 +0900

    [CHORE] MainActivity의 임시 View 전환 버튼 제거
    - MainActivity의 임시 View  전환 버튼 바인딩 제거
    - activity_main.xml의 임시  View 전환 버튼 View 제거

--- Frontend/app/src/main/java/com/with_runn/MainActivity.kt
+++ Frontend/app/src/main/java/com/with_runn/MainActivity.kt
@@ -1,17 +1,16 @@
 package com.with_runn
 
-import android.content.Intent
+import android.os.Build
 import android.os.Bundle
-import android.widget.Button
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.with_runn.databinding.ActivityMainBinding
-import com.with_runn.ui.mypage.MypageFollowersActivity
-import com.with_runn.ui.onboarding.OnboardingActivity
-import kotlin.jvm.java
 
+@RequiresApi(Build.VERSION_CODES.P)
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,18 +23,6 @@ class MainActivity : AppCompatActivity() {
         val navHost = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment;
         val navController = navHost.navController;
 
-        binding.bottomNavigationView.setupWithNavController(navController);
-
-        //버튼 클릭 시 OnboardingActivity로 이동
-        val button: Button = binding.startOnboardingActivityButton
-        button.setOnClickListener {
-            val intent = Intent(this, OnboardingActivity::class.java)
-            startActivity(intent)
-        }
-
-        binding.startMypageFollowersActivityButton.setOnClickListener {
-            val intent = Intent(this, MypageFollowersActivity::class.java)
-            startActivity(intent)
-        }
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
